73dd401156f97694735a28446d4999a4024923da
Воронков Никита <nikitavoronkov@yandex.ru>
2023-05-11 15:13:38 +0300

    global: add new code for ФЗ9

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,18 +1,69 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.validation.ValidationException;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.model.User;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserController {
-    private void addNewUser() {
+    static int userId;
+    final Map<Integer, User> userMap;
 
+    public UserController() {
+        userMap = new HashMap<>();
+        userId = 0;
     }
 
+    public Integer generateId() {
+        return ++userId;
+    }
+
+    @PostMapping
+    private User addNewUser(@Validated @RequestBody User user) {
+        for (User valueComparison : userMap.values()) {
+            if (valueComparison.getEmail().equals(user.getEmail())) {
+                throw new ValidationException("Фильм уже есть в нашей базе");
+            }
+        }
+        user.setId(generateId());
+        if (user.getLogin().contains(" ")) {
+            log.warn(user.getLogin());
+            throw new RuntimeException("Логин не может быть пустым");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        userMap.put(user.getId(), user);
+        log.trace("Добавлен пользователь {} ", user);
+        return user;
+    }
+
+    @PutMapping
     private void updateUser() {
 
     }
 
-    private List getListOfUsers() {
+    @GetMapping
+    private List<User> getListOfUsers() {
         return null;
-        
+
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/MinDateReleaseValidator.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class MinDateReleaseValidator implements ConstraintValidator<MinDateRelease, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate dateString, ConstraintValidatorContext context) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate minDateRelease = LocalDate.parse("1895-12-28", formatter);
+        return !dateString.isBefore(minDateRelease);
+    }
+}
