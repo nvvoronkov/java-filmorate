8a791d93498cfd717edca16eb37b5e87fb4db855
Воронков Никита <nikitavoronkov@yandex.ru>
2023-05-17 16:06:29 +0300

    feat: add new metod

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,18 +1,22 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
 import javax.validation.ValidationException;
 
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
@@ -32,7 +36,7 @@ public class UserController {
     }
 
     @PostMapping
-    private User addNewUser(@Validated @RequestBody User user) {
+    private User addNewUser(@Valid @RequestBody User user) {
         for (User valueComparison : userMap.values()) {
             if (valueComparison.getEmail().equals(user.getEmail())) {
                 throw new ValidationException("Фильм уже есть в нашей базе");
@@ -52,13 +56,25 @@ public class UserController {
     }
 
     @PutMapping
-    private void updateUser() {
-
+    public User updateUser(@Valid @RequestBody User user, BindingResult bindingResult)
+            throws JsonProcessingException {
+        int id = user.getId();
+        if (id == 0) {
+            log.warn("Отсутствует id");
+            throw new ValidationException("Отсутствует id");
+        } else if (!userMap.containsKey(id)) {
+            log.warn("Пользователь с id {} не найден", id);
+            throw new ValidationException("Пользователь с id " + id + " не найден");
+        } else {
+            userMap.put(id, user);
+            log.debug("Пользователь {} обновлен", userMap.get(id));
+        }
+        return user;
     }
 
     @GetMapping
     private List<User> getListOfUsers() {
-        return null;
-
+        log.debug("Текущее количество пользователей: {}", userMap.size());
+        return new ArrayList<>(userMap.values());
     }
 }
